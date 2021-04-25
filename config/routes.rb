83456dc8734da498280daa8eac0dac7109ede3c0
bhagathreddy123 <katta_reddy04@yahoo.com>
2021-04-25 11:02:44 -0700

    HeatMap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'weather/index'
- get 'weather/index'
- get 'weather/data', :defaults => { :format => 'json' }
+  get 'weather/index'
+  get 'weather/data', :defaults => { :format => 'json' }
+  get 'weather/heatmap'
+  get 'weather/heatmap_data', :defaults => { :format => 'json' }
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
